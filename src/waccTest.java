eacd84934c51690a00abbbfb538a6973fdccd822
Athithan Dharmaratnam <ad2816@sprite10.doc.ic.ac.uk>
2017-10-30 12:19:09 +0000

    [tp416] Made a wacc test file, need to fix grammar files

---
+++ src/waccTest.java
@@ -0,0 +1,19 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import antlr.*;
+
+public class waccTest {
+  public static void main(String[] args) throws Exception {
+    ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+    WaccLexer lexer = new WaccLexer(input);
+
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+    WaccParser parser = new WaccParser(tokens);
+
+    ParseTree tree = parser.program();
+    System.out.println(tree.toStringTree(parser));
+  }
+}
