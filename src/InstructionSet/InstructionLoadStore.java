3b0d6f27e098d9695add8d8220ba88b623a70b6f
Matthew Robinson <mjr316@matrix37.doc.ic.ac.uk>
2017-11-20 16:54:00 +0000

    continued with work on instructions, nearly finished

--- src/InstructionSet/InstructionLoadStore.java
+++ src/InstructionSet/InstructionLoadStore.java
@@ -1,4 +1,28 @@
 package InstructionSet;
 
+import java.util.List;
+
 public class InstructionLoadStore extends Instruction{
+
+  String op;
+  String reg;
+  String mem;
+  String block1;
+
+  public InstructionLoadStore(String op, String reg, String mem) {
+    this.op = op;
+    this.reg = reg;
+    this.mem = mem;
+  }
+
+  @Override
+  public void genInstruction(List<Instruction> instructions) {
+    StringBuilder builder = new StringBuilder("\t\t");
+    builder.append(op);
+    builder.append(" ");
+    builder.append(reg);
+    builder.append(" ");
+    builder.append(mem + "\n");
+    block1 = builder.toString();
+  }
 }
--- src/InstructionSet/InstructionStack.java
+++ src/InstructionSet/InstructionStack.java
@@ -1,4 +1,24 @@
 package InstructionSet;
 
-public class InstructionStack {
+import java.util.List;
+
+public class InstructionStack extends Instruction {
+
+  String reg;
+  String block1;
+  String op;
+
+  public InstructionStack(String op, String reg) {
+    this.reg = reg;
+    this.op = op;
+  }
+
+  @Override
+  public void genInstruction(List<Instruction> instructions) {
+    StringBuilder builder = new StringBuilder("\t\t");
+    builder.append(op);
+    builder.append(" ");
+    builder.append(reg + "\n");
+    block1 = builder.toString();
+  }
 }
