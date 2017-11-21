b3708f0fd80da303c02cbc9e0f1978821d8f9de7
Matthew Robinson <mjr316@sprite24.doc.ic.ac.uk>
2017-11-21 14:51:40 +0000

    started work on instruction for assigning literals and did instructions for while and print

---
+++ src/InstructionSet/InstructionAssignLit.java
@@ -0,0 +1,36 @@
+package InstructionSet;
+
+import Registers.RegisterARM;
+
+public class InstructionAssignLit extends Instruction{
+
+  String constant;
+  String lit;
+  String reg;
+  String block1;
+
+  public InstructionAssignLit(String constant, String lit){
+    this.constant = constant;
+    this.lit = lit;
+  }
+
+  public void registerAllocation(RegisterARM reg){
+    this.reg = reg.name();
+  }
+
+  @Override
+  public void genInstruction() {
+    if (lit.equals("char")) {
+      lit = "'" + lit + "'";
+    } else if (lit.equals("string")) {
+      
+    }
+
+
+    StringBuilder builder = new StringBuilder("\t\tMOV ");
+    builder.append(reg);
+    builder.append(" #");
+    builder.append(constant);
+    builder.append("\n");
+  }
+}
