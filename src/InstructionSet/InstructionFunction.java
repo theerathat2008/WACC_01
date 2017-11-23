a6d457ba5e2949047aa08d386fa930d3021a6748
Matthew Robinson <mjr316@sprite24.doc.ic.ac.uk>
2017-11-23 20:15:20 +0000

    fixed error with brackets in program and function

--- src/InstructionSet/InstructionFunction.java
+++ src/InstructionSet/InstructionFunction.java
@@ -13,10 +13,10 @@ public class InstructionFunction extends Instruction {
     StringBuilder builder = new StringBuilder("\t");
     builder.append("f_" + name);
     builder.append("\n");
-    builder.append("\t\tPUSH (lr)\n");
+    builder.append("\t\tPUSH {lr}\n");
     block1 = builder.toString();
 
-    StringBuilder builder2 = new StringBuilder("\t\tPOP (pc)\n");
+    StringBuilder builder2 = new StringBuilder("\t\tPOP {pc}\n");
     builder2.append("\t\t.ltorg\n");
     block2 = builder2.toString();
   }
