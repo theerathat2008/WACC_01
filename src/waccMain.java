b93c1298715d369e809bdb1a0cfcffd36d8fd76b
Athithan Dharmaratnam <ad2816@matrix25.doc.ic.ac.uk>
2017-12-06 21:21:58 +0000

    Implemented expression bracketing system to use ordering of opcodes

--- src/waccMain.java
+++ src/waccMain.java
@@ -7,20 +7,86 @@ import VisitorClass.AST_VisitCodeGen;
 import VisitorClass.AST_VisitSemantic;
 import VisitorClass.waccVisitor;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.Interval;
 import org.antlr.v4.runtime.tree.*;
 
 import antlr.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class waccMain {
+
+  public static boolean isInt(char in){
+    return Character.isLetterOrDigit(in);
+  }
 
 
   public static void main(String[] args) throws Exception {
 
-    System.out.println("-- Compiling...");
-
+    int fileCharLength = 999999999;
     CharStream input = CharStreams.fromStream(System.in);
 
-    WaccLexer lexer = new WaccLexer(input);
+    String inputText = input.getText(new Interval(0,fileCharLength));
+
+    System.out.println(inputText);
+
+    List<Character> tempArray = new ArrayList<Character>();
+
+    for(char c : inputText.toCharArray()){
+      tempArray.add(c);
+    }
+    int counter = 0;
+
+    for(int i = 0; i < inputText.length(); i++){
+      char currChar = inputText.charAt(i);
+
+      if(currChar == '*' || currChar == '/' || currChar == '%'){
+        System.out.println(currChar + " at " + i);
+        boolean hitBracket = false;
+        for(int j = i + counter; j > 0; j--){
+          if(isInt(tempArray.get(j)) && !hitBracket){
+            tempArray.add(j , '(');
+            break;
+          } else if(tempArray.get(j) == ')'){
+            hitBracket = true;
+          } else if(tempArray.get(j) == '('){
+            tempArray.add(j, '(');
+            hitBracket = false;
+            break;
+          }
+        }
+        for(int j = i + counter; j < inputText.length(); j++){
+          if(isInt(tempArray.get(j))){
+            tempArray.add(j + 1, ')');
+            break;
+          }
+        }
+        counter += 2;
+      }
+    }
+
+    StringBuilder interString = new StringBuilder();
+    for(int i = 0; i < tempArray.size(); i++){
+//      if(tempArray.get(i) == '(' && tempArray.get(i+2) == ')'){
+//        System.out.println(tempArray.get(i+1));
+//        interString.append(tempArray.get(i + 1));
+//        i = i + 2;
+//      } else {
+        interString.append(tempArray.get(i));
+//      }
+    }
+
+    String outputText = interString.toString();
+    System.out.println(outputText);
+
+    System.out.println("-- Compiling...");
+
+
+    CharStream inputString = CharStreams.fromString(outputText);
+
+    WaccLexer lexer = new WaccLexer(inputString);
 
     CommonTokenStream tokens = new CommonTokenStream(lexer);
 
