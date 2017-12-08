27c7010c39b041fe1bc8e3d8b2282d2b9cefef66
Prashan Nesarajah <pn716@matrix34.doc.ic.ac.uk>
2017-12-08 13:49:04 +0000

    Fixed broken expr test case

--- src/ASTNodes/AST_Stats/AST_StatRead.java
+++ src/ASTNodes/AST_Stats/AST_StatRead.java
@@ -191,25 +191,8 @@ public class AST_StatRead extends AST_Stat {
   public void acceptPreProcess(RegisterAllocation regAlloc) {
     //Set a flag for acceptRegister in statVarDecl using a list in registerallocation to declare the var on the stack
     // since it is used in read and the statarraylitrhs assembly code works with stacks
-    String identName = "UNDEFINED";
-
-    if (ast_statAssignLHS instanceof AST_StatIdentLHS) {
-      AST_StatIdentLHS ast_statIdentLHS = (AST_StatIdentLHS)ast_statAssignLHS;
-      identName = ast_statIdentLHS.getIdentName();
-    } else if (ast_statAssignLHS instanceof AST_StatPairElemLHS) {
-      AST_StatPairElemLHS ast_statIdentLHS = (AST_StatPairElemLHS) ast_statAssignLHS;
-   //   identName = ast_statIdentLHS.identifier.getName();
-      System.out.println("IDENTNAME1*********************:  " + identName);
-
-      identName = ast_statIdentLHS.getAst_expr().getIdentifier().toString();
-      System.out.println("IDENTNAME2*********************:  " + identName);
-
-      identName = ast_statIdentLHS.getAst_expr().getIdentifier().toString();
-      System.out.println("IDENTNAME3*********************:  " + identName);
-
-      identName = ast_statIdentLHS.getAst_expr().getIdentifier().getName();
-      System.out.println("IDENTNAME4*********************:  " + identName);
-    }
+    AST_StatIdentLHS ast_statIdentLHS = (AST_StatIdentLHS)ast_statAssignLHS;
+    String identName = ast_statIdentLHS.getIdentName();
 
 
     regAlloc.addToStackOnlyVar(identName);
